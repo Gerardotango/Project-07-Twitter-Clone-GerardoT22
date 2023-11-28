5574c8b1738a804e674215f6c25c0526bf7b56f6
Gerardotango <135970009+Gerardotango@users.noreply.github.com>
2023-11-28 08:02:02 +0000

    add change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Layout from "./components/layout.jsx";
 
 import('./style/reset.css');
 import('./style/App.css');
-// fhhffjudududgg
 export default function App() {
   return (
     <Layout>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-const title = 'Hello gerardo';
-const  autor = 'Tango';
-const style = {color : 'yellow'}
+
+
 function Home() {
   return (
     <main className="timeline">
-      <h1> {title} <b style = {style}>{autor}</b> 
-      </h1>
+
+    
+     
+
+
     </main>
-    // hrhfruiuirui
+
   );
 }
 
