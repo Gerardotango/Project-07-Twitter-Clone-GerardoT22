544d82f4839864f2a0e06f0c3be33f594b9e862b
Gerardotango <135970009+Gerardotango@users.noreply.github.com>
2023-12-02 13:26:08 +0000

    complimente

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import Header from '../components/header';
+import HeroSection from '../components/herosection';
+import Herosection2 from '../components/herosection2';
 
 function Home() {
   return (
     <main className="timeline">
 
-    
-     
+      <Header />
+      <HeroSection />
+      <Herosection2 />
+
+
 
 
     </main>
